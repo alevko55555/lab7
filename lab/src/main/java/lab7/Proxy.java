2ccfd35bec9719850c8de9b91ca85c6bc1e5a8ab
alevko55555 <alevko55555@gmail.com>
2019-12-20 17:25:21 +0300

    Scripted auto-commit on change (2019-12-20 17:25:21) by gitwatch.sh

--- lab/src/main/java/lab7/Proxy.java
+++ lab/src/main/java/lab7/Proxy.java
@@ -1,9 +1,18 @@
 package lab7;
 
+import org.zeromq.SocketType;
 import org.zeromq.ZMQ;
 
 public class Proxy {
     public void exampleProxy() {
-        ZMQ.Context
+        ZMQ.Context context = ZMQ.context(1);
+        ZMQ.Socket fronted = context.socket(SocketType.XSUB);
+        fronted.bind("tcp://*:5559");
+        ZMQ.Socket backend = context.socket(SocketType.XPUB);
+        backend.bind("tcp://*:5560");
+        ZMQ.proxy(fronted, backend, null);
+        fronted.close();
+        backend.close();
+        context.term();
     }
 }
