2a7e81b5a41a00e5c6742b7260b03a3dc3b8dd4a
alevko55555 <alevko55555@gmail.com>
2019-12-27 21:14:40 +0300

    Scripted auto-commit on change (2019-12-27 21:14:40) by gitwatch.sh

--- lab/src/main/java/lab7/Storage.java
+++ lab/src/main/java/lab7/Storage.java
@@ -8,8 +8,8 @@ public class Storage {
     private static int right;
 
     public static void main(String[] args){
-
+        Scanner in = new Scanner(System.in);
+        String temp = in.nextLine();
+        left = Integer.parseInt(in.nextLine());
     }
-
-    
 }
