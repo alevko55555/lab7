2b1a2745b5bd9d2ba137f5a634bd8bcc0e69816c
alevko55555 <alevko55555@gmail.com>
2019-12-27 21:05:18 +0300

    Scripted auto-commit on change (2019-12-27 21:05:18) by gitwatch.sh

---
+++ lab7_1/src/main/java/lab7/Client.java
@@ -0,0 +1,33 @@
+package lab7;
+import org.zeromq.SocketType;
+import org.zeromq.ZContext;
+import org.zeromq.ZMQ;
+import org.zeromq.ZMsg;
+
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Client {
+    public static void main(String[] args){
+        //ZMQ.Context context = ZMQ.context(1);
+        ZContext context = new ZContext();
+        ZMQ.Socket client = context.createSocket(SocketType.REQ);
+        client.setHWM(0);
+        client.connect("tcp://localhost:5555");
+
+        System.out.println("Client started");
+
+        Scanner in = new Scanner(System.in);
+
+        while (true) {
+            String msg = in.nextLine();
+            ZMsg req = new ZMsg();
+            req.addString(msg);
+            req.send(client);
+            ZMsg ans = ZMsg.recvMsg(client);
+            String str = ans.popString();
+            System.out.println(str);
+            ans.destroy();
+        }
+    }
+}
