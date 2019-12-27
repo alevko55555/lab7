83c0445a9e6201677458c689cebfac470e1f7e70
alevko55555 <alevko55555@gmail.com>
2019-12-27 22:47:39 +0300

    Scripted auto-commit on change (2019-12-27 22:47:39) by gitwatch.sh

--- lab7_1/src/main/java/lab7/Proxy.java
+++ lab7_1/src/main/java/lab7/Proxy.java
@@ -1,10 +1,12 @@
 package lab7;
 
-import org.zeromq.*;
+//import org.zeromq.*;
 
-import java.net.Socket;
+//import java.net.Socket;
 import java.util.HashMap;
 import java.util.Map;
+import org.zeromq.*;
+import org.zeromq.ZMQ.Socket;
 
 public class Proxy {
     private  static HashMap<ZFrame, GetStorageData> storageDataHashMap;
