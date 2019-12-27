df26846a586297b7a2919670009bcf1b32d553ae
alevko55555 <alevko55555@gmail.com>
2019-12-27 10:03:31 +0300

    Scripted auto-commit on change (2019-12-27 10:03:31) by gitwatch.sh

--- lab/src/main/java/lab7/GetStorageData.java
+++ lab/src/main/java/lab7/GetStorageData.java
@@ -5,5 +5,25 @@ public class GetStorageData {
     private int right;
     private long time;
 
-    
+    public GetStorageData(int left, int right, long time){
+        this.left = left;
+        this.right = right;
+        this.time = time;
+    }
+
+    public int getLeft() {
+        return left;
+    }
+
+    public int getRight() {
+        return right;
+    }
+
+    public long getTime() {
+        return time;
+    }
+
+    public void setTime(long time) {
+        this.time = time;
+    }
 }
