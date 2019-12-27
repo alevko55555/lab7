86904e16cb45410cf775d1332e3c6df2831eedd9
alevko55555 <alevko55555@gmail.com>
2019-12-27 21:53:20 +0300

    Scripted auto-commit on change (2019-12-27 21:53:20) by gitwatch.sh

--- lab7_1/src/main/java/lab7/Proxy.java
+++ lab7_1/src/main/java/lab7/Proxy.java
@@ -3,6 +3,7 @@ package lab7;
 import org.zeromq.SocketType;
 import org.zeromq.ZContext;
 import org.zeromq.ZFrame;
+import org.zeromq.ZMQ;
 
 import java.net.Socket;
 import java.util.HashMap;
@@ -13,8 +14,8 @@ public class Proxy {
     public static void main(String[] args){
         storageDataHashMap = new HashMap<>();
         ZContext context = new ZContext();
-        Socket frontend = context.createSocket(SocketType.ROUTER);
-        Socket backend = context.createSocket(SocketType.ROUTER);
+        ZMQ.Socket frontend = context.createSocket(SocketType.ROUTER);
+        ZMQ.Socket backend = context.createSocket(SocketType.ROUTER);
         frontend.setHWM(0);
     }
 }
