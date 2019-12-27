1050ff83d850b50a4d6921f7ed3cdc9593a1ec6f
alevko55555 <alevko55555@gmail.com>
2019-12-27 10:00:11 +0300

    Scripted auto-commit on change (2019-12-27 10:00:11) by gitwatch.sh

--- lab/src/main/java/lab7/Client.java
+++ lab/src/main/java/lab7/Client.java
@@ -5,7 +5,7 @@ import org.zeromq.ZMQ;
 import java.util.Scanner;
 import java.util.StringTokenizer;
 
-public class Main {
+public class Client {
     public static void main(String[] args){
         ZMQ.Context context = ZMQ.context(1);
         ZMQ.Socket subscriber = context.socket(SocketType.SUB);
