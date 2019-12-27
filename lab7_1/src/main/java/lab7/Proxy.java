7c5e2f94a4486bd9a7b694645a7c65306764a011
alevko55555 <alevko55555@gmail.com>
2019-12-27 22:04:43 +0300

    Scripted auto-commit on change (2019-12-27 22:04:43) by gitwatch.sh

--- lab7_1/src/main/java/lab7/Proxy.java
+++ lab7_1/src/main/java/lab7/Proxy.java
@@ -34,7 +34,7 @@ public class Proxy {
                 for(Map.Entry<ZFrame, GetStorageData> entry : storageDataHashMap.entrySet()) {
                     if(entry.getValue().getLeft() <= Integer.parseInt(strings[1]) && entry.getValue().getRight() > Integer.parseInt(strings[1])) {
                         if(!found) {
-                            if(System.currentTimeMillis() - entry.g)
+                            if(System.currentTimeMillis() - entry.getValue().getTime())
                         }
                     }
                 }
